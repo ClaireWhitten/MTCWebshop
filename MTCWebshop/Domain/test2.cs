e96d23079c319fd8d1f7de908cd060d91951ee6b
kaouchi Mohamed <med_kaouchi@hotmail.com>
2021-08-10 09:53:24 +0200

    test2

---
+++ MTCWebshop/Domain/test2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTCWebshop.Domain
+{
+    public class test2
+    {
+        public int ID { get; set; }
+    }
+}
